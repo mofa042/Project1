15551fae52df6678321a2577d2a6f4ab2cccc38a
Fouad <mo.fa042@outlook.com>
2025-08-24 10:17:01 +0300

    Modifying module1.py to add time greetings

--- module1.py
+++ module1.py
@@ -1,2 +1,16 @@
+from datetime import datetime
+
 def greet(name):
-    print(f"Hello, {name}!")
+    hour = int(datetime.now().strftime(format="%H"))
+    greet = ""
+    
+    if (hour >= 5):
+        greet = "Morning"
+    if (hour >= 12):
+        greet = "Afternoon"
+    if (hour >= 17):
+        greet = "Evening"
+    if (hour >= 21 or hour < 5):
+        greet = "Night"
+        
+    print(f"Good {greet}, {name}!")
