bb0f499b5a0dce7fe9cdc5c3bbc8077aa08b327a
Fouad <mo.fa042@outlook.com>
2025-08-24 11:41:05 +0300

    changes

--- app.py
+++
@@ -1,8 +0,0 @@
-from module1 import *
-
-print("Hello World!")
-greet("Mohamad")
-
-print("This is an update!")
-
-farewell("Mohamad")
--- app1.py
+++ app1.py
@@ -4,5 +4,6 @@ print("Hello World!")
 greet("Mohamad")
 
 print("This is an update!")
+print("This is another update!")
 
 farewell("Mohamad")
